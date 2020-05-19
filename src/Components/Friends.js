1c9cd1fda120439f7ff46db4fda1f57ab39b21fb
Thomas Smith (Tom) <tomytank@users.noreply.github.com>
2020-05-19 13:30:38 +0200

    Setup Nav, Childs, List, Routes
    Initial working app with routes, links and small formatting.

--- src/Components/Friends.js
+++ src/Components/Friends.js
@@ -1,1 +1,16 @@
 import React from "react";
+
+import friendsData from "./FriendsData";
+
+const Friends = () => {
+  return (
+    <div>
+      <p>Friends -></p>
+      {friendsData.map(friend => {
+        return <h3>{friend.name}</h3>;
+      })}
+    </div>
+  );
+};
+
+export default Friends;
---
+++ src/Components/FriendsData.js
@@ -0,0 +1,28 @@
+const friends = [
+  {
+    id: 1,
+    name: "Roger McCarey",
+    nickname: "Roger the Dodge",
+    description: "Creative. Helpful. Sarcastic.",
+    quote: "All the little dots...",
+    specialAbilities: ["Persistance", "Determined", "Spending thrift"]
+  },
+  {
+    id: 2,
+    name: "Tim Needham",
+    nickname: "Big mouth",
+    description: "Disco maniac. Music. Psychedelic images.",
+    quote: ["Let's get a drink", "well you would wouldn't you..."],
+    specialAbilities: ["Fluid", "Getting on", "Friendly advantages"]
+  },
+  {
+    id: 3,
+    name: "John ",
+    nickname: "Johny bring your girlfiend home",
+    description: "I met this girl today...",
+    quote: "C'mon, it'll be fun...",
+    speicalAbilities: ["Late", "Finds money", "Makes  happy"]
+  }
+];
+
+export default friends;
